bf7ac8b05d051b09f150d16e17ef21033e53531a
The Galactic Senate <80652317+friknob325@users.noreply.github.com>
2023-03-18 21:05:34 -0700

    finished the carousel
    add the stuff larr

--- main.js
+++ main.js
@@ -2,6 +2,35 @@
 let respondBtn = document.getElementById("respondbtn");
 let moodBtns = document.getElementsByClassName("moodBtn"); 
 
+var multipleCardCarousel = document.querySelector(
+    "#carouselExampleControls"
+  );
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+    var cardWidth = $(".carousel-item").width();
+    var scrollPosition = 0;
+    $("#carouselExampleControls .carousel-control-next").on("click", function () {
+      if (scrollPosition < carouselWidth - cardWidth * 4) {
+        scrollPosition += cardWidth;
+        $("#carouselExampleControls .carousel-inner").animate(
+          { scrollLeft: scrollPosition },
+          600
+        );
+      }
+    });
+    $("#carouselExampleControls .carousel-control-prev").on("click", function () {
+      if (scrollPosition > 0) {
+        scrollPosition -= cardWidth;
+        $("#carouselExampleControls .carousel-inner").animate(
+          { scrollLeft: scrollPosition },
+          600
+        );
+      }
+    });
+  } else {
+    $(multipleCardCarousel).addClass("slide");
+  }
+
 document.getElementById("other").onclick = (e) => {
     location.href = "bubblesgame.html";
 }
